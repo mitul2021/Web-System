bc080e51223db99d7574a89d22ff1c062a20bf9f
Jason Ong <jmsong1@sheffield.ac.uk>
2021-02-26 11:33:08 +0000

    Created the table for admins.

---
+++ db/admin_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE admin (
+    id INTEGER PRIMARY KEY,
+    email TEXT,
+    password TEXT,
+    first_name TEXT,
+    surname TEXT,
+    age INTEGER,
+    gender TEXT,
+    contact_number INTEGER,
+    profile_text TEXT,
+);
