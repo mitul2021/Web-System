00d60c83ae5abacc56dbb5d667ebc50af538981d
Aleksander Osikowicz <amosikowicz1@sheffield.ac.uk>
2021-03-03 16:42:17 +0000

    After registration user is redirected to the signin page.

--- controllers/register.rb
+++ controllers/register.rb
@@ -2,8 +2,8 @@ get "/register" do
    erb :register
 end
 
-post "/register" do
-    @variable = params["password"]
-    puts @variable
-   erb :register
-end
+# post "/register" do
+#     @variable = params["password"]
+#     puts @variable
+#    erb :register
+# end
