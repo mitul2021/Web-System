09021aadffc84496bf6669cdbad6cb09fa087922
Zac Jones <zjones1@sheffield.ac.uk>
2021-03-09 13:30:41 +0000

    Populated users.db // Users and course tables

--- db/degrees_faculties_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE degrees_faculties (
-    id INTEGER PRIMARY KEY,
-    degree TEXT,
-    faculty TEXT,
-);
--- db/pairings_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE pairings (
-    pairing_id INTEGER PRIMARY KEY,
-    mentee_id INTEGER,
-    mentor_id INTEGER,
-    start_date TEXT,
-    end_date TEXT,
-);
