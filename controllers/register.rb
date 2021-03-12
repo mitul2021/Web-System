c2d20603aac52999825eeb303d7bc45d04afc637
Aleksander Osikowicz <amosikowicz1@sheffield.ac.uk>
2021-03-12 15:05:05 +0000

    Now user can register and its record is stored in the DB, however yet without user_type

--- controllers/register.rb
+++ controllers/register.rb
@@ -2,8 +2,21 @@ get "/register" do
    erb :register
 end
 
-# post "/register" do
-#     @variable = params["password"]
-#     puts @variable
-#    erb :register
-# end
+post "/register" do
+    #user validation
+    @user = User.new
+    @user.email = params["email"]
+    @user.password = params["password"]
+    @user.user_type = params["user_type"].to_s #becomes a nil for now, needs debugging
+    #waiting for the reply on the discussion board
+    
+    if(@user.valid? && !@user.exist?) #those functions need to be implemented
+        
+        @user.save_changes
+        puts "SUCCESS, There is new entry in the DB"
+        
+        redirect "/register-successfully"
+    else 
+        erb :register
+    end
+end
