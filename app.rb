663783f504dbd24fa0dad8fb8fb3561c2ae8af33
Zac Jones <zjones1@sheffield.ac.uk>
2021-03-12 11:41:50 +0000

    Add handling for 404 and / urls

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 #Gems
 require "sinatra"
 require "require_all"
+require "sinatra/reloader" # Must be removed during demonstration and final project
 
 include ERB::Util
 
--- controllers/signin.rb
+++ controllers/signin.rb
@@ -1,3 +1,13 @@
+get "/" do
+    redirect "/signin" unless session[:logged_in]
+    erb :index
+end
+
+get "/logout" do
+    session.clear
+    erb :logout
+end   
+   
 get "/signin" do
     #@
     
