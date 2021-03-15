6580cf528dbd54f37a8842e356094f19f266dc5d
Aleksander Osikowicz <amosikowicz1@sheffield.ac.uk>
2021-03-15 10:52:34 +0000

    Now register captures the user type

--- controllers/register.rb
+++ controllers/register.rb
@@ -5,10 +5,7 @@ end
 post "/register" do
     #user validation
     @user = User.new
-    @user.email = params["email"]
-    @user.password = params["password"]
-    @user.user_type = params["user_type"].to_s #becomes a nil for now, needs debugging
-    #waiting for the reply on the discussion board
+    @user.load(params)
     
     if(@user.valid? && !@user.exist?) #those functions need to be implemented
         
--- models/User.rb
+++ models/User.rb
@@ -1,16 +1,18 @@
 
 class User < Sequel::Model
     
-    def load(email, password)
-        self.email = email
-        self.password = password
+    def load(params)
+        self.email = params.fetch("email"," ").strip
+        self.password = params.fetch("password"," ").strip
+        self.user_type = params.fetch("user_type"," ").strip
     end
     
     def exist?
         first_user = User.first(email: email)
+        
         if first_user.nil?
             return false
-        elsif first_user.password = self.password
+        elsif first_user.password == self.password
             return true
         else
             return false
