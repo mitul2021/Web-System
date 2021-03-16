9ba4d273a4607916141b3ced4860db49f7a74940
Joel Hogg <jhogg2@sheffield.ac.uk>
2021-03-16 12:03:50 +0000

    fixed process-log-out

--- controllers/process-log-out.rb
+++ controllers/process-log-out.rb
@@ -1,4 +1,4 @@
-get "process-log-out" do #using post to stop acsess from anywhere but logout buttons
+get "/process-log-out" do #using post to stop acsess from anywhere but logout buttons
     if session[:signedin]
         #if/when we add cookies they should be cleared here
         session[:signedin] = false
