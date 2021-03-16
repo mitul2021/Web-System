d317ad046b9581b6186335c188adcb5d0082e655
Aleksander Osikowicz <amosikowicz1@sheffield.ac.uk>
2021-03-16 12:14:26 +0000

    Setting up session variables in the login controller so that multichoicesurvey presents right contents.

--- controllers/login.rb
+++ controllers/login.rb
@@ -18,7 +18,17 @@ post "/login" do
     @user.load_login(params) #passes the params from the form straight to the model
     if @user.valid_login?
         session[:loggedin] = true
+        
+        @user = User.first(email: @user.email)
+        session[:user_type] = @user.user_type
+        
+        
+        
         puts "Login successful"
+        puts "LOGIN CONTROLLER:"
+        puts "My role is #{session[:user_type]}"
+        puts "My login in status is : #{session[:loggedin]}"
+        
         redirect "/index"
     else
         session[:loggedin] = false
--- controllers/multichoicesurvey.rb
+++ controllers/multichoicesurvey.rb
@@ -1,14 +1,7 @@
 get "/multichoicesurvey" do
-    @role = "mentee" #Change to nil after uncommenting line 5
     @courses = Courses.all
-    #user_status is the role cookie 
-    #@role = request.cookies["user_status"] #expects a 'role' session cookie to be set when user logs in
+    @role = session[:user_type]
+    #puts "My role is #{@role}"
+    #puts "My login in status is : #{session[:loggedin]}"
     erb :multichoicesurvey
 end
-#i think this code may need to go in process-multichoicesurvey.rb
-#nvm it doesnt cuz this code needs to run when the page opens so it knows to display the questionaire for mentee or mentor
-
-#this should work now, it just sets up the @role so the view can query if the currently signed in user is a mentee or mentor and
-    #display the right questions
-
-#may be helpful to deny viewing the page if role is nil
