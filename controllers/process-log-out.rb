218d398952ea99f4948df3dfe27a0a2444190a7f
Aleksander Osikowicz <amosikowicz1@sheffield.ac.uk>
2021-03-17 13:32:30 +0000

    Cleaning up the repository: removing unnecessary views and controllers. It's functionality will be replaced by pop-ups implemented by cookies.

--- controllers/process-log-out.rb
+++
@@ -1,8 +0,0 @@
-get "/process-log-out" do #using post to stop acsess from anywhere but logout buttons
-    if session[:loggedin]
-        #if/when we add cookies they should be cleared here
-        erb :logoutsuccessful
-    else
-        erb :logoutunsuccessful
-    end
-end
