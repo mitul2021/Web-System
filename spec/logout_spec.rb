54fac3b3aa18af195c2e4133e0e50b1f77c245da
Jason Ong <jmsong1@sheffield.ac.uk>
2021-03-18 00:04:52 +0000

    Created rspec test for logout page

---
+++ spec/logout_spec.rb
@@ -0,0 +1,24 @@
+require "rspec"
+require "rack/test"
+
+require_relative "../controllers/logout"
+
+RSpec.describe "Logout Page" do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  describe "GET /logout" do
+    it "has a status code of 200 (OK)" do
+      get "/logout"
+      expect(last_response.status).to eq(200)
+    end
+
+    it "says 'Logout was successful'" do
+      get "/logout"
+      expect(last_response.body).to eq("Logout was successful")
+    end
+  end
+end
