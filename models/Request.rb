9a693003a5b7953f371c101b209873ff6e881453
Aleksander Osikowicz <amosikowicz1@sheffield.ac.uk>
2021-03-19 16:48:21 +0000

    Removing unwanted comments.

--- models/Request.rb
+++ models/Request.rb
@@ -13,28 +13,10 @@ class Request < Sequel::Model
     
     def exist?
         db_requests_array = Request.all
-        
         db_requests_array.each do |record|
             return true if ((record.mentor_id==self.mentor_id) && (record.mentee_id==self.mentee_id))
         end
-        
         return false
-        
-#         is_nil = (!db_request.nil?)
-#         puts "Have we found the DB record?: #{is_nil}"
-        
-#         puts "OUR MENTOR ID: #{mentor_id}"
-#         puts "OUR MENTEE ID: #{mentee_id}"
-#         puts
-        
-#         puts "DB's MENTOR ID: #{db_request.mentor_id}"
-#         puts "DB's MENTEE ID: #{db_request.mentee_id}"
-#         puts
-#         same_mentee_id = db_request.mentee_id.eql?(self.mentee_id)
-#         puts "FIRST ID #{self.mentee_id}; SECOND ID #{db_request.mentee_id}"
-#         puts "Do we have the same mentee ids? : #{same_mentee_id}"
-#         #if user is nil return false, if db_user exists and it has the same password return true
-#         return (!db_request.nil?) && (db_request.mentee_id.eql?(self.mentee_id))
     end
     
     
