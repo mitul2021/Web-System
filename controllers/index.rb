78af788459e8ea0f884d7a6c70a8e1724f8e24dc
Joel Hogg <jhogg2@sheffield.ac.uk>
2021-03-19 10:22:39 +0000

    removed information non-specific to user type in index.erb

--- controllers/index.rb
+++ controllers/index.rb
@@ -16,6 +16,8 @@ get "/index" do
         @just_logged_in = true if cookie == "true" #used by view to display the message
         response.delete_cookie("make-login-popup") #deleting the cookie
         
+        @user_type = session[:user_type]
+        
         erb :index
     else
         response.set_cookie("redirected-popup", value: 'true')
