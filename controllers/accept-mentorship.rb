b6e5fa2be207bc09ee9d43f878ee8f165c7b2c7a
Mitul Patel <mpatel16@sheffield.ac.uk>
2021-03-20 14:20:35 +0000

    Accepting mentorship now works

--- controllers/accept-mentorship.rb
+++ controllers/accept-mentorship.rb
@@ -1,3 +1,15 @@
 get "/accept" do
+    
+    puts "This is mentor's ID: #{session[:user_id]}"
+    
+    if(session[:user_type].eql?("mentor"))
+        @pairID = params["id"].to_i #Retrieves the id from the params hash and sets it to pairID and converts to an integer
+        puts "Mentor entered"
+        if Pair.id_exists?(@pairID)
+            @pair = Pair[@pairID]
+            @pair.status = 1
+            @pair.save_changes(validate: false) # Updates DB without validation (not secure so will have to be changed for iteration 2)
+        end
+    end
     redirect "/index"
 end
--- models/Pair.rb
+++ models/Pair.rb
@@ -19,5 +19,14 @@ class Pair < Sequel::Model
         return false
     end
     
+    def self.id_exists?(id)
+        return false if id.nil?
+        puts "id is definitely not nil"
+        #return false unless id.is_a? Integer # checks that the id is an integer
+        #puts "id is definitely an integer"
+        return false if Pair[id.to_i].nil? # check the database has a record with this id
+        puts "Found in the database, success!"
+        return true
+    end
     
 end
