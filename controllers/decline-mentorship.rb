832762d17e050e51300f475cb99d60535b1b9689
Aleksander Osikowicz <amosikowicz1@sheffield.ac.uk>
2021-03-20 15:50:16 +0000

    Record is deleted from the DB once mentor cancels relationship with the mentee

--- controllers/decline-mentorship.rb
+++ controllers/decline-mentorship.rb
@@ -1,3 +1,19 @@
 get "/decline" do
+    puts "This is mentor's ID: #{session[:user_id]}"
+    
+    if(session[:user_type].eql?("mentor"))
+        @pairID = params["id"].to_i #Retrieves the id from the params hash and sets it to pairID and converts to an integer
+        puts "Mentor want to delete pair"
+        if Pair.id_exists?(@pairID)
+            @pair = Pair[@pairID]
+            @pair.delete
+            #@pair.save_changes(validate: false) maybe we don't need that
+        end
+    end
     redirect "/index"
 end
+
+
+
+    
+    
