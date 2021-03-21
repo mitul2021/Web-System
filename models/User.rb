71866f4471f2c7c329bfc81b1f769cb9da384918
Mitul Patel <mpatel16@sheffield.ac.uk>
2021-03-21 14:35:32 +0000

    Fixing typo for the error message if the repeated password is empty for when registering

--- models/User.rb
+++ models/User.rb
@@ -68,7 +68,7 @@ class User < Sequel::Model
         validate
         errors.add("email", "email cannot be empty") if email.empty?
         errors.add("password", "password cannot be empty") if password.empty?
-        errors.add("password_repeat", "password repeacannot be empty") if @password_repeat.empty?
+        errors.add("password_repeat", "password repeat cannot be empty") if @password_repeat.empty?
         errors.add("password_repeat", "the passwords are not the same") if @password_repeat!=password
         errors.add("password", "password must be at least 8 characters long") if password.length<8
         errors.add("email", "there exist user with such email address") if exist?
