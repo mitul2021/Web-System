e318cc5580bf66fe6cc874a091128a86682161f2
Mitul Patel <mpatel16@sheffield.ac.uk>
2021-03-22 10:42:07 +0000

    Fixed code so that it uses the database file users.sqlite3 for production by default

--- db/db.rb
+++ db/db.rb
@@ -3,23 +3,8 @@ require "sequel"
 require "sqlite3"
 
 
-
-# # find the path to the database
-#db_dir_path = File.dirname(__FILE__)
-#db_path = "#{db_dir_path}/#{name}.sqlite3"
-
-# # find the path to the log
-#log_dir_path = "#{db_dir_path}/log/"
-#log_path = "#{log_dir_path}/#{name}.log"
-
-# # create log directory if it does not exist
-#Dir.mkdir(log_dir_path) unless File.exist?(log_dir_path)
-
-# # set up the Sequel database instance
-#DB = Sequel.sqlite(db_path, logger: Logger.new(log_path))
-
-#name = "acceptance_test" 
-name = "users"
+# ensuring that you are using the users.sqlite3 database for production
+name = ENV.fetch("APP_ENV", "users")
 
 # # find the path to the database
 db_dir_path = File.dirname(__FILE__)
