3caca0bb37b2fb0d7f28dbdb5539ba7271a2ef20
Mitul Patel <mpatel16@sheffield.ac.uk>
2021-03-27 09:11:49 +0000

    Removed the adminhome view and controller as index will handle the display for the admin and likewise for the controller

--- controllers/adminhome.rb
+++
@@ -1,8 +0,0 @@
-get "/adminhome" do
-    if session[:loggedin]
-        erb :adminhome
-    else
-        response.set_cookie("redirected-popup", value: 'true')
-        redirect "/login"
-    end
-end
