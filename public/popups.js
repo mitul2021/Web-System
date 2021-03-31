c01c43602dbf324f532acb8cab25c7d455972b5c
Mitul Patel <mpatel16@sheffield.ac.uk>
2021-03-31 12:18:55 +0000

    Fixing javascript, added new file for javascript functions.

---
+++ public/popups.js
@@ -0,0 +1,22 @@
+
+function cancelMentorship() {
+    return confirm("Are you sure you want to cancel mentorship? This action cannot be undone.");
+}
+
+function cancelRequest() {
+    return confirm("Are you sure you want to cancel this request? This action cannot be undone.");
+}
+
+function acceptRequest() {
+    return confirm("Are you sure you want to accept this request? This action cannot be undone.");
+}
+
+function denyRequest() {
+    return confirm("Are you sure you want to deny this request? This action cannot be undone.");
+}
+
+function requestMentorship() {
+    return confirm("Are you sure you want to request a mentorship? This action cannot be undone.");
+}
+
+
