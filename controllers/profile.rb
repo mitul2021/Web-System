aa5fb91506c36b2dc8d4f96af6ed85feffa55538
Mitul Patel <mpatel16@sheffield.ac.uk>
2021-04-02 09:42:30 +0000

    Dynamic URLs have been improved for security purposes. Only admins can view all users. Changed controller names to accept and decline so that it is not restrictive to certain functionality only. User can now go back to the previous page from the not_found view. Changed heading from Your Requests to Incoming Request in the home page for the mentor

--- controllers/profile.rb
+++ controllers/profile.rb
@@ -1,8 +1,52 @@
 get "/profile/*" do
+    
+    # Getting the user type
+    @role = session[:user_type]
+    
     # * = wildcard for user ID
     id_num = params[:splat][0]
-    #not_found unless
+    
+    # the id_num is the number in the url for that profile 
     @user = User[id_num]
+    
+    # If you are a mentee
+    if @role.eql?("mentee")
+        
+        # If you want to see other mentee or admin profiles
+        if @user.user_type.eql?("mentee") || @user.user_type.eql?("admin")
+            # Redirect them to the not found page as they have no access right
+            redirect "/not_found"
+        end
+    
+    elsif @role.eql?("mentor")
+        
+        # List of pairs where the logged in user is a mentor
+        @pairs = Pair.where(mentor_id: session[:user_id])
+        
+        # New array that will only contain the IDs that the mentor can view (the IDs will be retrieved by iterating through the pairs like below)
+        allowedIDs = Array.new
+        
+        @pairs.each do |pair|
+            # Adding the ids of your mentees only (the ids that you can view)
+            # .to_s because we are comparing strings in a later comparison
+            allowedIDs.push(pair.mentee_id.to_s)
+        end
+        
+        puts "Here are the allowed IDs: #{allowedIDs}"
+        
+        # Unless the mentor can view the IDs of the mentee, redirect to not_found page
+        if !(allowedIDs.include?(id_num.to_s))
+            redirect "/not_found"
+        end
+        
+    elsif @role.eql?("admin")
+        # The admins are not restricted to certain profiles as they can view any profile including other admins
+    
+    else
+       # For debugging purposes
+       puts "Error: you are not a mentee, mentor or admin."
+    end
+    
     erb :profile
     
 end
--- public/popups.js
+++ public/popups.js
@@ -1,20 +1,24 @@
-
+// For when a mentee or mentor clicks on Cancel Mentorship
 function cancelMentorship() {
     return confirm("Are you sure you want to cancel mentorship? This action cannot be undone.");
 }
 
+// For when a mentee clicks on Cancel Request
 function cancelRequest() {
     return confirm("Are you sure you want to cancel this request? This action cannot be undone.");
 }
 
+// For when a mentor clicks on Accept Request from a mentee
 function acceptRequest() {
     return confirm("Are you sure you want to accept this request? This action cannot be undone.");
 }
 
+// For when a mentor clicks on Deny Request from a mentee
 function denyRequest() {
     return confirm("Are you sure you want to deny this request? This action cannot be undone.");
 }
 
+// For when a mentee clicks on Request Mentorship
 function requestMentorship() {
     return confirm("Are you sure you want to request a mentorship? This action cannot be undone.");
 }
