08e1c8bc4476593f0fedba9d540b0a0083794f62
Mitul Patel <mpatel16@sheffield.ac.uk>
2021-04-02 10:49:41 +0000

    Added table for admins that are currently enrolled in the system in the User List view. Not given is displayed if any fields for the user are blank

--- controllers/userlist.rb
+++ controllers/userlist.rb
@@ -1,8 +1,8 @@
 get "/userlist" do
     if session[:loggedin]
-        # Body is currently skeleton, will be changed later
         @mentees = User.where(user_type: "mentee")
         @mentors = User.where(user_type: "mentor")
+        @admins = User.where(user_type: "admin")
         erb :userlist
     else
         response.set_cookie("redirected-popup", value: 'true')
