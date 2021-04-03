f7869e9d3d9635d9d4b19f057898db85277afb04
Aleksander Osikowicz <amosikowicz1@sheffield.ac.uk>
2021-04-03 10:21:59 +0000

    Removing comments from the model validation

--- models/Userinterest.rb
+++ models/Userinterest.rb
@@ -2,8 +2,6 @@ class Userinterest < Sequel::Model
 
     def validate
         super
-        puts "We are in validate method"
-        #errors.add(:interest_id, 'cannot be empty') if interest_id.blank?
         errors.add(:interest_id, 'is not an integer') if !(self.interest_id.is_a? Integer)
     end
 
