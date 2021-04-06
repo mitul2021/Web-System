bf47907fff0f211cef290642745e57c852945948
Mitul Patel <mpatel16@sheffield.ac.uk>
2021-04-06 12:29:30 +0000

    Now user can be redirected to the change account details page from the profile create page. Corresponding input boxes depending on if the user is logged in or not is yet to be implemented alongside sending the admin a request through the Request Changes button

--- public/changeaccountdetails.js
+++ public/changeaccountdetails.js
@@ -1,14 +1,20 @@
+// For when the user clicks on the button My Email
 function desiredEmail() {
     var displayEmail = document.getElementById("displayemailbox");
     
+    document.getElementById("changetext").style.display="none";
+    document.getElementById("change_email").style.display="none";
     document.getElementById("displayemailbox").removeAttribute("hidden");
     document.getElementById("change_password").style.display="none";
     document.getElementById("displaychangesbutton").removeAttribute("hidden");
 }
 
+// For when the user clicks on the button My Password
 function desiredPassword() {
     var displayPassword = document.getElementById("displaypasswordbox");
     
+    document.getElementById("changetext").style.display="none";
+    document.getElementById("change_password").style.display="none";
     document.getElementById("displaypasswordbox").removeAttribute("hidden");
     document.getElementById("change_email").style.display="none";
     document.getElementById("displaychangesbutton").removeAttribute("hidden");
