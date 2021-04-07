37550e61540a3cea691caf49746574c546fc9994
Aleksander Osikowicz <amosikowicz1@sheffield.ac.uk>
2021-04-07 11:31:10 +0000

    Handling form type one in the controller  changeaccountdetails.rb

--- controllers/changeaccountdetails.rb
+++ controllers/changeaccountdetails.rb
@@ -9,13 +9,13 @@ post "/changeaccountdetails" do
 
     case type
     when 1 
-        loadFormDataType1(params)
+        handleType1(params)
     when 2 
-        loadFormDataType2(params)    
+        handleType2(params)    
     when 3 
-        loadFormDataType3(params)
+        handleType3(params)
     when 4 
-        loadFormDataType4(params)
+        handleType4(params)
     else
         "You gave me #{type} -- I have no idea what to do with that."
     end
@@ -32,11 +32,57 @@ post "/changeaccountdetails" do
     end
 end
 
+def handleType1(params)
+    data = loadFormDataType1(params) #new_email,password
+    #validation
+    
+    #password check
+    if data[1]==User[session[:user_id]].password
+        puts "The password is correct"
+    else
+        puts "The password is incorrect"
+        #possibly add cookie here, and try again or sth
+        return
+    end
+
+    #at this stage validation done
+    @new_request = Userrequest.new
+    @new_request.request_id = 1 #code for changing email
+    @new_request.user_id = session[:user_id]
+    @new_request.request_data = data[0] #for the request we are entering desired email of the user
+    @new_request.status = 0 #code pending request
+
+    if @new_request.valid?
+        puts "New request is valid saving changes..."
+        @new_request.save_changes
+    else
+        puts "New request is invalid sth gone wrong."
+        #place holder for displaying error messages
+        
+    end
+    
+
+end
+
+def handleType2(params)
+    data = loadFormDataType2(params) #password, new_password, repeat_password
+
+end
+
+def handleType3(params)
+    data = loadFormDataType3(params) #new_email, recovery_code
+end
+
+def handleType4(params)
+    data = loadFormDataType4(params) #password, repeat_password, recovery_code
+end
+
 def loadFormDataType1(params)
-    email = params.fetch("email"," ").strip
+    new_email = params.fetch("new_email"," ").strip
     password = params.fetch("password"," ").strip
 
-    puts "FORM NO.1 email: #{email}, password: #{password}"
+    puts "FORM NO.1 new_email: #{new_email}, password: #{password}"
+    return [new_email,password]
 end
 
 def loadFormDataType2(params)
@@ -45,13 +91,15 @@ def loadFormDataType2(params)
     repeat_password = params.fetch("repeat_password"," ").strip
 
     puts "FORM NO.2 password: #{password}, new_password: #{new_password}, repeat_password: #{repeat_password}"
+    return [password,new_password,repeat_password]
 end
 
 def loadFormDataType3(params)
-    email = params.fetch("email"," ").strip 
+    new_email = params.fetch("new_email"," ").strip 
     recovery_code = params.fetch("recovery_code"," ").strip
 
-    puts "FORM NO.3 email: #{email}, recovery_code: #{recovery_code}"
+    puts "FORM NO.3 new_email: #{new_email}, recovery_code: #{recovery_code}"
+    return [new_email,recovery_code]
 end
 
 def loadFormDataType4(params)
@@ -60,6 +108,7 @@ def loadFormDataType4(params)
     recovery_code = params.fetch("recovery_code"," ").strip
 
     puts "FORM NO.4 password: #{password}, repeat_password: #{repeat_password}, recovery_code: #{recovery_code}"
+    return [password,repeat_password,recovery_code]
 end
 
 
