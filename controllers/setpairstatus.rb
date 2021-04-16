4621de35288b83aaf8432d4bca0a24592bf5c1b3
Aleksander Osikowicz <amosikowicz1@sheffield.ac.uk>
2021-04-16 09:13:01 +0000

    Rewriting model for Pair and writing new controller for setting pair status

---
+++ controllers/setpairstatus.rb
@@ -0,0 +1,46 @@
+post "/setpairstatus" do
+
+    status = params.fetch("status","").strip.to_i
+    redirect_path = params.fetch("path","").strip
+
+    case status
+    when 0
+        #we dont have a pair yet, we need to fetch data to create one
+        mentor_id = params.fetch("mentor_id","").strip.to_i
+        mentee_id = params.fetch("mentee_id","").strip.to_i
+
+        new_pair = Pair.new
+        new_pair.mentor_id = mentor_id
+        new_pair.mentee_id = mentee_id
+        new_pair.status = 0 #initail pair status
+
+        if new_pair.valid? #when status zero there shouldnt be any records in the db for that mentor
+            puts "Newly created pair with status 0 is valid"
+            new_pair.save_changes
+        else
+            puts "Newly created pair with status 0 is NOT valid"
+            puts pair.errors
+        end
+
+    when 1,2,3,4,5,6
+        #pair already exists
+        pair_id = params.fetch("pair_id","").strip.to_i
+        pair = Pair[pair_id]
+        pair.status=status
+
+        if pair.valid? #when status > zero there should be only one pair in the DB
+            puts "Pair after changing its status to #{status} is valid."
+            pair.save_changes
+        else
+            puts "Pair after changing its status to #{status} is NOT valid."
+            puts pair.errors
+        end
+
+    else
+        puts "Retrieved unhadled status."
+    end
+
+
+    redirect "#{redirect_path}"
+
+end
--- models/Pair.rb
+++ models/Pair.rb
@@ -4,34 +4,60 @@ class Pair < Sequel::Model
     def validate
         super
         
-        if !(mentor_id.is_a? Integer)
-            errors.add("mentor_id", "mentor_id is not a number")
-            
-        end
-        
-        if !(mentee_id.is_a? Integer)
-            errors.add("mentee_id", "mentee_id is not a number")
-        end
-        
-        if exist?
-            errors.add("general", "there already exists such a request in DB") 
-        end
+        #if !(mentor_id.is_a? Integer)
+        #    errors.add("mentor_id", "mentor_id is not a number")
+        #    
+        #end
+        #
+        #if !(mentee_id.is_a? Integer)
+        #    errors.add("mentee_id", "mentee_id is not a number")
+        #end
+        #
+        #if exist?
+        #    errors.add("general", "there already exists such a request in DB") 
+        #end
+#
+        ##checking if the mentor already has 10 or more ongoing requests
+        #if (Pair.where(mentor_id: mentor_id, status: 0).count()>=10) 
+        #    errors.add("tooManyRequests","this mentor has to many requests")
+        #end
+        # 
+        #return errors.empty? #if there are no errors we are good to go, and returns true
 
-        #checking if the mentor already has 10 or more ongoing requests
-        if (Pair.where(mentor_id: mentor_id, status: 0).count()>=10) 
-            errors.add("tooManyRequests","this mentor has to many requests")
-        end
-         
-        return errors.empty? #if there are no errors we are good to go, and returns true
+        errors.add(:mentee_id, 'is nil') if !mentee_id || mentee_id.nil? 
+        errors.add(:mentee_id, 'mentee id cannot be negative') if mentee_id<=0
+
+        errors.add(:mentor_id, 'is nil') if !mentor_id || mentor_id.nil? 
+        errors.add(:mentor_id, 'mentor id cannot be negative') if mentor_id<=0
+        
+        errors.add(:status, 'status can only be integer in range [0,6]') if !(status.between?(0,6))
+
+        errors.add(:pair_id, 'by status 0 number of pairs for one mentee should be 0') if (status==0 && numOfPairs!=0)
+        errors.add(:pair_id, 'by status in range [1,6] number of pairs for one mentee should be 1') if (status.between?(1,6) && numOfPairs!=1)
     end
     
     def exist?
-        db_pair_array = Pair.all
-        db_pair_array.each do |record|
-            # Returns true if your id is already in the record for pairs
-            return true if ((record.mentee_id==self.mentee_id))
+        #db_pair_array = Pair.all
+        #db_pair_array.each do |record|
+        #    # Returns true if your id is already in the record for pairs
+        #    return true if ((record.mentee_id==self.mentee_id))
+        #end
+        #return false
+
+        #this function is meant to check if there exist more then one db record coneccted with same mentee
+
+        
+    end
+
+    #this function returns number of relationships of one mentee
+    #should be equal to 0 if current status is 0,
+    #should be equal to 1 if current status is [1,6]
+    def numOfPairs #for one mentee
+        count = 0
+        Pair.all.each do |record|
+            count += 1 if (record.mentee_id==self.mentee_id)
         end
-        return false
+        return count
     end
     
     def self.id_exists?(id)
