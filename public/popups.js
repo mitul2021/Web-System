0c9df89ee3c9a6fe8f623cbc132ae3a37d03fcf3
Mitul Patel <mpatel16@sheffield.ac.uk>
2021-04-17 15:07:24 +0000

    New javascript functions are now implemented. Further refining javascript functions in index.erb

--- public/popups.js
+++ public/popups.js
@@ -3,45 +3,96 @@ function requestMeeting() {
     return confirm("Are you sure you want to request a meeting with this mentor?");
 }
 
-// For when a mentee has to agree on cancelling their mentorship with the mentor
-function agreeOnCancelling() {
-    return confirm("Do you agree to cancelling your mentorship with this mentor?")
+
+// For when a mentor clicks on Accept Meeting Request
+function acceptMeetingRequest() {
+    return confirm("Are you sure you want to accept a meeting request from this mentee?");
 }
+
+
+// For when a mentor clicks on Decline Meeting Request or Cancel Meeting Request
+function declineMeetingRequest() {
+    return confirm("Are you sure you want to decline a meeting request from this mentee?")
+}
+
 
 // For when a mentee wants to cancel their ongoing Meeting Request
 function cancelMeetingRequest() {
-    return confirm("Are you sure you want to cancel your meeting request with this mentor?")
+    return confirm("Are you sure you want to cancel your meeting request with this mentor?");
 }
+
+
+// For when a mentee clicks on Request Mentorship
+function requestMentorship() {
+    return confirm("Are you sure you want to request a mentorship?");
+}
+
+
+// For when a mentor clicks on Accept Mentorship Request
+function acceptMentorshipRequest() {
+    return confirm("Are you sure you want to accept a mentorship request from this mentee?");
+}
+
+
+// For when a mentor clicks on Decline Mentorship Request
+function declineMentorshipRequest() {
+    return confirm("Are you sure you want to decline a mentorship request from this mentee?");
+}
+
 
 // For when a mentee or mentor clicks on Cancel Mentorship
 function cancelMentorship() {
-    return confirm("Are you sure you want to cancel mentorship? This action cannot be undone.");
+    return confirm("Are you sure you want to cancel mentorship?");
 }
+
+
+// For when a mentee wants to clicks on Cancel Application to cancel the application process for mentoring
+function cancelApplication() {
+    return confirm("Are you sure you want to cancel your application at this stage?");
+}
+
+
+// For when a mentee wants to clicks on Cancel Mentorship Request
+function cancelMentorshipRequest() {
+    return confirm("Are you sure you want to cancel your ongoing Mentorship Request?");
+}
+
+
+// For when a mentee clicks on Cancel ongoing Request
+function cancelOngoingRequest() {
+    return confirm("Are you sure you want to cancel your ongoing request?");
+}
+
+
+// For when a mentee has to agree on cancelling their mentorship with the mentor
+function agreeOnCancelling() {
+    return confirm("Do you agree to cancelling your mentorship with this mentor?");
+}
+
 
 // For when a mentee clicks on Cancel Request
 function cancelRequest() {
     return confirm("Are you sure you want to cancel this request? This action cannot be undone.");
 }
 
+
 // For when a mentor clicks on Accept Request from a mentee
 function acceptRequest() {
     return confirm("Are you sure you want to accept this request? This action cannot be undone.");
 }
+
 
 // For when a mentor clicks on Decline Request from a mentee
 function declineRequest() {
     return confirm("Are you sure you want to decline this request? This action cannot be undone.");
 }
 
-// For when a mentee clicks on Request Mentorship
-function requestMentorship() {
-    return confirm("Are you sure you want to request a mentorship? This action cannot be undone.");
-}
 
 // For when a admin clicks on Suspend User
 function suspendUser() {
     return confirm("Are you sure you want to suspend this user?");
 }
+
 
 // For when a admin clicks on Restore User
 function restoreUser() {
