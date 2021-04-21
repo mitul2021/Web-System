8caaf0f538d514c86d728825b203f07271c13d00
Zac Jones <zjones1@sheffield.ac.uk>
2021-04-21 09:49:26 +0100

    Remove very old DB retrieval code

--- models/User.rb
+++ models/User.rb
@@ -41,48 +41,6 @@ class User < Sequel::Model
             self.id = id
         end
     end
-    
-    
-    def load_details_mentee(params)
-
-        
-        self.first_name = params.fetch("first_name"," ").strip
-        self.surname = params.fetch("surname"," ").strip
-        self.year_of_birth = params.fetch("year_of_birth"," ").strip
-        self.gender = params.fetch("gender"," ").strip
-        self.contact_number = params.fetch("contact_number"," ").strip
-        self.deg_id = params.fetch("deg_id"," ").strip
-        self.deg_year = params.fetch("deg_year"," ").strip
-        self.profile_text = params.fetch("profile_text"," ").strip
-    
-    end
-    
-    
-    def load_details_mentor(params)
-
-        self.first_name = params.fetch("first_name"," ").strip
-        self.surname = params.fetch("surname"," ").strip
-        self.year_of_birth = params.fetch("year_of_birth"," ").strip #to be changed later age -> year_of_birth
-        self.gender = params.fetch("gender"," ").strip
-        self.contact_number = params.fetch("contact_number"," ").strip
-        self.job_deg_cosmetic_name = params.fetch("job_deg_cosmetic_name"," ").strip
-        #self.deg_id = params.fetch("deg_id"," ").strip
-        self.profile_text = params.fetch("profile_text"," ").strip
-        #self.deg_year = params.fetch("deg_year"," ").strip
-        #self.major_interest = params.fetch("major_interest"," ").strip
-    end
-    
-    
-    def load_details_admin(params)
-
-        self.first_name = params.fetch("first_name"," ").strip
-        self.surname = params.fetch("surname"," ").strip
-        self.year_of_birth = params.fetch("year_of_birth"," ").strip #to be changed later age -> year_of_birth
-        self.gender = params.fetch("gender"," ").strip
-        self.contact_number = params.fetch("contact_number"," ").strip
-        self.profile_text = params.fetch("profile_text"," ").strip
-    
-    end
   
     def valid_details?
         validate
