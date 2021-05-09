f0d1d1428b2e360b13ced90f59ab83b8dbb63296
Jason Ong <jmsong1@sheffield.ac.uk>
2021-05-09 23:41:50 +0000

    Completed testing valid_login? method

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,7 +4,7 @@ require "rack/test"
 require_relative "spec_helper"
 require_relative "../app"
 
-RSpec.describe "Index page" do
+RSpec.describe "User.rb" do
   include Rack::Test::Methods
 
   def app
@@ -22,6 +22,27 @@ RSpec.describe "Index page" do
     context "when password is empty" do
       it "returns false" do
           @user = User.new(:username => "username", :password => "", :email => "sample@sheffield.ac.uk")        
+          expect(@user.valid_login?).to eq(false)
+      end
+    end
+      
+    context "when no details are entered" do
+      it "returns false" do
+          @user = User.new(:username => "", :password => "", :email => "")        
+          expect(@user.valid_login?).to eq(false)
+      end
+    end
+      
+    context "when email is entered but not username" do
+      it "returns false" do
+          @user = User.new(:username => "", :password => "", :email => "sample@sheffield.ac.uk")        
+          expect(@user.valid_login?).to eq(false)
+      end
+    end
+    
+    context "when username is entered but not email" do
+      it "returns false" do
+          @user = User.new(:username => "23575324", :password => "", :email => "")        
           expect(@user.valid_login?).to eq(false)
       end
     end
